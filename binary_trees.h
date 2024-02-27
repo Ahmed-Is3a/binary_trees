b28bc064021fb1c3b14bed8a2e35621658883791
ahmed <ahmedissaten@gmail.com>
2024-02-27 10:24:46 +0200

    protect double inclution

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,5 @@
-
+#ifndef BINARY
+#define BINARY
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -9,10 +10,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -24,3 +25,5 @@ void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+#endif
