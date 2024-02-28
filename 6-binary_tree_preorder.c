99a3ce3f94d1b1d916ac8260a4bc6e8887eaa822
ahmed <ahmedissaten@gmail.com>
2024-02-28 09:11:09 +0200

    in-order travers

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,11 +13,11 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	/* Call func for the current node */
-	func(tree->n);
 
 	/* Traverse the left subtree */
 	binary_tree_preorder(tree->left, func);
 
 	/* Traverse the right subtree */
 	binary_tree_preorder(tree->right, func);
+	func(tree->n);
 }
