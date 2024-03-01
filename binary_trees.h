2dbb0b16d7928b9c11ac9ece228ac5c3ec3bbe85
ahmed <ahmedissaten@gmail.com>
2024-03-01 22:03:36 +0200

    tree balance

--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree); /* 14 */
 
 #endif
