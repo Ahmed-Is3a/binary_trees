fe30914b7e5dcb7a12f55718f4e247e138fc7378
ahmed <ahmedissaten@gmail.com>
2024-03-01 18:47:08 +0200

    betty fixed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,21 +10,21 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-    binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *grandparent = node->parent->parent;
 
-    if (grandparent->left == node->parent)
-    {
-        if (grandparent->right != NULL)
-            return (grandparent->right);
-    }
-    else
-    {
-        if (grandparent->left != NULL)
-            return (grandparent->left);
-    }
+	if (grandparent->left == node->parent)
+	{
+		if (grandparent->right != NULL)
+			return (grandparent->right);
+	}
+	else
+	{
+		if (grandparent->left != NULL)
+			return (grandparent->left);
+	}
 
-    return (NULL);
+	return (NULL);
 }
